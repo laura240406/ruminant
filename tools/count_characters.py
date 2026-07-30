23256e1a4c7a02ea85777abfb0795d7798d00a2a
Laura Kirsch <laurakirsch240406@gmail.com>
2026-07-30 20:29:09 +0200

    added character counter tool

---
+++ tools/count_characters.py
@@ -0,0 +1,20 @@
+import os
+import tqdm
+
+content = ""
+bar = tqdm.tqdm()
+
+for root, _, files in os.walk(os.path.join(os.path.dirname(os.path.dirname(__file__)), "ruminant")):
+    for file in files:
+        file = os.path.join(root, file)
+
+        with open(file, "r") as f:
+            lines = f.read().split("\n")
+
+            for line in lines:
+                content += line.strip()
+                bar.update(1)
+
+bar.close()
+
+print(f"{len(content)} character(s).")
