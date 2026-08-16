a153b1770357196f815a0ebce4d636366aaaec93
Laura Kirsch <laurakirsch240406@gmail.com>
2026-08-16 12:54:16 +0200

    fixed self test in ci

--- ruminant/test_core.py
+++ ruminant/test_core.py
@@ -1,18 +1,18 @@
 from . import tests as ts
 import traceback
 import os
-import sys
 
 
 # tests main
 def main():
+    slim = False
+
     # try to get terminal width so we can display the status on the right
     try:
         width = os.get_terminal_size().columns
         assert width > 40
     except Exception:
-        print("\033[0;;41mTerminal is too narrow.", file=sys.stderr)
-        exit()
+        slim = True
 
     # get total test count to print it
     test_counts = sum([len(x) for x in ts.tests.values()])
@@ -24,7 +24,10 @@ def main():
     ig = 1
     jg = len(ts.tests)
     for group, tests in ts.tests.items():
-        print(f"\033[0;30;47mGroup {group} [{ig}/{jg}]\033[0m")
+        if slim:
+            print(f"Group {group} [{ig}/{jg}]")
+        else:
+            print(f"\033[0;30;47mGroup {group} [{ig}/{jg}]\033[0m")
         ig += 1
 
         # index for test progress report
@@ -38,12 +41,18 @@ def main():
             try:
                 test()
 
-                print(" " * (width - len(line) - 7), end="")
-                print("\033[92msuccess\033[0m")
+                if slim:
+                    print("success")
+                else:
+                    print(" " * (width - len(line) - 7), end="")
+                    print("\033[92msuccess\033[0m")
                 success += 1
             except Exception:
-                print(" " * (width - len(line) - 6), end="")
-                print("\033[91mfailed\033[0m")
+                if slim:
+                    print("failed")
+                else:
+                    print(" " * (width - len(line) - 6), end="")
+                    print("\033[91mfailed\033[0m")
                 print("Error was:")
                 traceback.print_exc()
 
@@ -54,7 +63,10 @@ def main():
                 # add failed name
                 failures[group].append(name)
 
-    print(f"\033[0;30;47mSummary: [{success}/{test_counts}]\033[0m")
+    if slim:
+        print(f"Summary: [{success}/{test_counts}]")
+    else:
+        print(f"\033[0;30;47mSummary: [{success}/{test_counts}]\033[0m")
 
     # print failed names per group
     if success != test_counts:
