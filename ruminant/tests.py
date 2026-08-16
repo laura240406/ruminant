338f0dd421f3a0b83b5753375bd1beca90aad8e6
Laura Kirsch <laurakirsch240406@gmail.com>
2026-08-16 14:33:38 +0200

    added curve25519 test

--- ruminant/tests.py
+++ ruminant/tests.py
@@ -926,3 +926,40 @@ def argon2id():
         crypto.argon2(b"\x01" * 32, b"\x02" * 32, 4, 32, 4, 32, "id").hex(),
         "e93820f3bea5f680d17351abc63a2b06a76152bc7643b4dec49fbf1eb5eec562",
     )
+
+
+@test("Cryptography", "Curve25519")
+def curve25519():
+    # Alice public
+    assert_eq(
+        crypto.curve25519(
+            b"\x09" + bytes(31), bytes.fromhex("77076d0a7318a57d3c16c17251b26645df4c2f87ebc0992ab177fba51db92c2a")
+        ).hex(),
+        "8520f0098930a754748b7ddcb43ef75a0dbf3a0d26381af4eba4a98eaa9b4e6a",
+    )
+
+    # Bob public
+    assert_eq(
+        crypto.curve25519(
+            b"\x09" + bytes(31), bytes.fromhex("5dab087e624a8a4b79e17f8b83800ee66f3bb1292618b6fd1c2f8b27ff88e0eb")
+        ).hex(),
+        "de9edb7d7b7dc1b4d35b61c2ece435373f8343c85b78674dadfc7e146f882b4f",
+    )
+
+    # Alice shared
+    assert_eq(
+        crypto.curve25519(
+            bytes.fromhex("de9edb7d7b7dc1b4d35b61c2ece435373f8343c85b78674dadfc7e146f882b4f"),
+            bytes.fromhex("77076d0a7318a57d3c16c17251b26645df4c2f87ebc0992ab177fba51db92c2a"),
+        ).hex(),
+        "4a5d9d5ba4ce2de1728e3bf480350f25e07e21c947d19e3376f09b3c1e161742",
+    )
+
+    # Bob shared
+    assert_eq(
+        crypto.curve25519(
+            bytes.fromhex("8520f0098930a754748b7ddcb43ef75a0dbf3a0d26381af4eba4a98eaa9b4e6a"),
+            bytes.fromhex("5dab087e624a8a4b79e17f8b83800ee66f3bb1292618b6fd1c2f8b27ff88e0eb"),
+        ).hex(),
+        "4a5d9d5ba4ce2de1728e3bf480350f25e07e21c947d19e3376f09b3c1e161742",
+    )
